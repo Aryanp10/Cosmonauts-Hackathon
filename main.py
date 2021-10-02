124c4cfe461a96863f61f619bd9cd8796fd47ae9
Aryanp10 <aaryanparajulee@gmail.com>
2021-10-02 15:44:55 -0500

    del

--- main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-@app.route('/home')
-def home_page():
-    return render_template('index.html')
-#print("bitch test")
